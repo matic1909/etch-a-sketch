506b7035b80a78c343ae713aa5aea91895a9b565
Nils Matic <matic1909@gmail.com>
2022-04-28 09:53:27 +0200

    added rainbow mode and clear button

--- index.js
+++ index.js
@@ -1,30 +1,59 @@
 const container = document.querySelector('.container');
 const gridButton = document.querySelector('.generate-grid-button');
 const toggleColorButton = document.querySelector('.toggle-color');
+const clearButton = document.querySelector('.clear');
 const colorDisplay = document.querySelector('.color');
+
+const rainbowColors = [
+  [255, 0, 0],
+  [255, 127, 0],
+  [255, 255, 0],
+  [127, 255, 0],
+  [0, 255, 0],
+  [0, 255, 127],
+  [0, 255, 255],
+  [0, 127, 255],
+  [0, 0, 255],
+  [127, 0, 255],
+  [255, 0, 255],
+  [255, 0, 127],
+];
+const modes = ['black', 'random', 'rainbow'];
+let currentMode = 0;
+let colorValues = [0, 0, 0];
+let rainbow = false;
+let rainbowIndex = 0;
 let randomColor = false;
 
 gridButton.addEventListener('click', (e) => {
   const gridSize = parseInt(prompt('How many squares?'));
   generateGrid(gridSize);
+  currentMode = 0;
 });
 
 toggleColorButton.addEventListener('click', (e) => {
-  randomColor = !randomColor;
-  if (randomColor) {
-    colorDisplay.textContent = 'RANDOM';
-  } else {
-    colorDisplay.textContent = 'BLACK';
-  }
+  currentMode = (currentMode + 1) % modes.length;
+  colorDisplay.textContent = `${modes[currentMode].toUpperCase()}`;
+});
+
+clearButton.addEventListener('click', (e) => {
+  container.childNodes.forEach((div) => {
+    div.style.backgroundColor = '';
+  });
 });
 
 const handleMouseEnter = (e) => {
-  if (randomColor) {
-    color = getRandomRGB();
-  } else {
-    color = getColorString([0, 0, 0]);
+  const mode = modes[currentMode];
+  if (mode === 'black') {
+    colorValues = [0, 0, 0];
   }
-  e.target.style.backgroundColor = color;
+  if (mode === 'random') {
+    getRandomRGB();
+  }
+  if (mode === 'rainbow') {
+    getNextColorInRainbow();
+  }
+  e.target.style.backgroundColor = getColorString(colorValues);
 };
 
 const generateGrid = (size) => {
@@ -47,7 +76,12 @@ const getRandomRGB = () => {
   const g = Math.floor(Math.random() * 255);
   const b = Math.floor(Math.random() * 255);
 
-  return `rgb(${r}, ${g}, ${b})`;
+  colorValues = [r, g, b];
+};
+
+const getNextColorInRainbow = () => {
+  colorValues = rainbowColors[rainbowIndex];
+  rainbowIndex = (rainbowIndex + 1) % rainbowColors.length;
 };
 
 generateGrid(16);
